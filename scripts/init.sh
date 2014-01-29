637cf051e74f1062672072de12997d9d2ca37f5f
Onsi Fakhouri <pair+onsi@pivotallabs.com>
2014-01-28 21:36:06 -0700

    init

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,6 +2,8 @@
 
 # this could be more automated easily.. I suck at bash
 # also, this assumes gocart is on $PATH, we should also fix that
+# also, to really nail this, we'll need to be able to tell gocart to skip "test-only" packages
+# also, gocart needs to not remove things from the .lock (or have a mode where it doesn't do that)
 
 git submodule update --init
 
